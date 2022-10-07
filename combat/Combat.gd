d2881b22216a970b8320fb748ec1c966f8511348
JarettRoman <afrocious@gmail.com>
2022-10-07 14:35:37 -0400

    project cleanup

--- combat/Combat.gd
+++ combat/Combat.gd
@@ -2,8 +2,7 @@ extends Node2D
 
 onready var enemy: Node2D = $Combatants/Ghost
 
+
 func _on_Attack_button_up() -> void:
-	var new_health: int = enemy.health - 1
+	var new_health: int = enemy.health - 3
 	enemy.set_health(new_health)
-
-
